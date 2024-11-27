4bdd01600d3ec9454d5b0e9350e836784f05f340
gnommon131313 <5s.ach@mail.ru>
2024-11-27 17:00:01 +0300

    none

--- ShopBot/tgbot/handlers/admin.py
+++
@@ -1,6 +0,0 @@
-from telebot import TeleBot
-from telebot.types import Message
-
-
-def some_start(self, message: Message, bot: TeleBot) -> None:
-    bot.send_message(message.chat.id, text="Hello, admin!")
--- ShopBot/tgbot/utils/states.py
+++
@@ -1,7 +0,0 @@
-from telebot.handler_backends import State, StatesGroup
-
-
-class User(StatesGroup):
-    get_name = State()
-    get_phone = State()
-    get_address = State()
